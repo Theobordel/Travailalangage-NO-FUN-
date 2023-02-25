d15f5417be5447d71775f8f102837ae765878705
nico30so <nico30saux@gmail.com>
2023-02-25 16:03:00 +0100

    lecteurImplementation

--- lecteur.py
+++ lecteur.py
@@ -7,16 +7,36 @@ info = (Q,alphabet,F)
 
 l = f.readline()
 
-
-def lecteur1(ligne,tablaux):
-    i =0
+def lecteurExt1(ligne,tablaux):
+    p = len(ligne) 
+    i = 0
+    tablaux = []
+    print(p,i)
+    caractaire = ""
+            
     while ligne[i] != "{":
         i = i+1
-    i= i+1
-    while ligne[i] != "}":
-        if ligne[i] != ",":
-            tablaux.append(ligne[i])
-        i=i+1
+    while p > i:
+
+        i= i+1
+        if ligne[i] == "}":
+            break
+        if ligne[i]== ",":
+            tablaux.append(caractaire)
+            caractaire =""
+        else:
+            caractaire = caractaire + ligne[i]
+            print(caractaire,"toto")
+    tablaux.append(caractaire)    
+    return tablaux
+
+def lecteurExt(tablaux):
+    for i in range(3) :
+        l = f.readline()
+        info[i] = lecteurExt1(l,tablaux)
+
 #print(l) 
 #print(content)
-print(etas)
+print(l)
+print(lecteurExt(info))
+
