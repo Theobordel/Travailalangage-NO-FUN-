b0f4bf4678b6561c1f01f78247031638df0754cd
nico30so <nico30saux@gmail.com>
2023-03-03 16:55:11 +0100

    debus_de_la_gestion_des_exeption

--- lecteur.py
+++ lecteur.py
@@ -1,9 +1,15 @@
-f = open('test.txt','r')
+
+try :
+    f = open('test.txt','r')
+except :
+    print("fichier nas pas pus etres")
+    exit()
 
 Q = []
 alphabet = []
 F = []
-info = [Q,alphabet,F]
+Qo =[]
+info = [Q,alphabet,F,Qo]
 
 #l = f.readline()#
 
@@ -11,30 +17,33 @@ def lecteurExt1(ligne,tablaux):
     p = len(ligne) 
     i = 0
     tablaux = []
-    print(p,i,ligne)
     caractaire = ""
-            
-    while ligne[i] != "{":
-        print(i)
-        i = i+1
-    while p > i:
+    try:
+        while ligne[i] != "{":
 
-        i= i+1
-        if ligne[i] == "}":
-            break
-        if ligne[i]== ",":
-            tablaux.append(caractaire)
-            caractaire =""
-        else:
-            caractaire = caractaire + ligne[i]
-            print(caractaire,"toto")
-    tablaux.append(caractaire)    
-    return tablaux
+            i = i+1
+        while p > i:
+
+            i= i+1
+            if ligne[i] == "}":
+                break
+            if ligne[i]== ",":
+                tablaux.append(caractaire)
+                caractaire =""
+            else:
+                caractaire = caractaire + ligne[i]
+        tablaux.append(caractaire)    
+        return tablaux
+    except:
+        print("ereur dans le format")
+        exit()
+    
 
 def lecteurExt(tablaux):
     for i in range(4) :
         l = f.readline()
         info[i] = lecteurExt1(l,tablaux)
+
     return info
 
 #print(l) 
