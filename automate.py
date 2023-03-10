f3b8c388b8767d79fa014f19ff642a764abfe8ce
SEP4000 <114220091+SEP4000@users.noreply.github.com>
2023-03-10 15:23:38 +0100

    Update automate.py

--- automate.py
+++ automate.py
@@ -1,38 +1,117 @@
-import random
+class automate:
 
-def affiche_tableau(tableau):
-    for i in range(len(tableau)):
-        for j in range(len(tableau[i])):
-            print(tableau[i][j], end=" ")
-        print()
-
-def calcul_score(tableau):
-    score = 0
-    for i in range(len(tableau)):
-        for j in range(len(tableau[i])):
-            if tableau[i][j] == "X":
-                score = 0
-            if tableau[i][j] == "O":
-                score = 0
-    return score
+    def __init__(self, alphabet):
+        self.states = []
+        self.transitions = {}
+        self.init = None
+        self.finals = []
+        self.alphabet = ""
+        for s in alphabet:
+            if s not in self.alphabet:
+                self.alphabet += s
 
 
+    def add_state(self, state, final = False):
+        if state in self.states:
+            print("error : state '" + state + "' already exists.")
+            return
+        self.transitions[state] = []
+        self.states.append(state)
+        if final:
+            self.finals.append(state)
+        return
 
-with open('automate.txt') as f:
-    lines = f.readlines()
+    def add_state(self, state, final = False):
+        if state in self.states:
+            print("error : state '" + state + "' already exists.")
+            return
+        self.transitions[state] = []
+        self.states.append(state)
+        if final:
+            self.finals.append(state)
 
 
-def stockage():
-    n=0
-    tableau = []
-    for i in range(n):
-        ligne = []
-        for j in range(n):
-            ligne.append(" ")
-        tableau.append(ligne)
-    return tableau
+
+    def valid_symbol(self, symbol):
+        if symbol not in self.alphabet: return False
+        return True
+    
+    def dst_state(self, src_state, symbol):
+        if src_state not in self.states:
+            print("error : the state '" + src_state + "' is not an existing state.")
+            return
+        for (s, dst_state) in self.transitions[src_state]:
+            if s == symbol:
+                return dst_state
+        return None
+    
+    def add_transition(self, src_state, symbol, dst_state):
+        if not self.valid_symbol(symbol):
+            print("error : the symbol '" + symbol + "' is not part of the alphabet.")
+            return
+        if src_state not in self.states:
+            print("error : the state '" + src_state + "' is not an existing state.")
+            return
+        if dst_state not in self.states:
+            print("error : the state '" + dst_state + "' is not an existing state.")
+            return
+    
+        if self.dst_state(src_state, symbol) != None:
+            print("error : the transition (" + src_state + ", " + symbol + ", ...) already exists.")
+            return
+    
+        self.transitions[src_state].append((symbol, dst_state))
+
+    def __str__(self):
+        ret = "FA :\n"
+        ret += "   - alphabet   : '" + self.alphabet + "'\n"
+        ret += "   - init       : " + str(self.init) + "\n"
+        ret += "   - finals     : " + str(self.finals) + "\n"
+        ret += "   - states (%d) :\n" % (len(self.states))
+        for state in self.states:
+            ret += "       - (%s)" % (state)
+            if len(self.transitions[state]) is 0:
+                ret += ".\n"
+            else:
+                ret += ret + ":\n"
+                for (sym, dest) in self.transitions[state]:
+                    ret += ret + "          --(%s)--> (%s)\n" % (sym, dest)
+        return ret
+
+    def __str__(self):
+        ret = "FA :\n"
+        ret += "   - alphabet   : '" + self.alphabet + "'\n"
+        ret += "   - init       : " + str(self.init) + "\n"
+        ret += "   - finals     : " + str(self.finals) + "\n"
+        ret += "   - states (%d) :\n" % (len(self.states))
+        for state in self.states:
+            ret += "       - (%s)" % (state)
+            if len(self.transitions[state]) is 0:
+                ret += ".\n"
+            else:
+                ret += ret + ":\n"
+                for (sym, dest) in self.transitions[state]:
+                    ret += ret + "          --(%s)--> (%s)\n" % (sym, dest)
+        return ret
+    
 
 
-f.close
+
+
+
+def run(automate, word):
+    current_state = automate.init
+
+    for symbol in word:
+        next_state = automate.dst_state(current_state, symbol)
+        if next_state is None:
+            return False
+
+        current_state = next_state
+        i = i+1
+
+    if current_state in automate.finals:
+        return True
+    return False
 
 
